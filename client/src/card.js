b092c475c7a69e8464a5f378b6dc7e43a56c40e6
Luke <npv.loc@gmail.com>
2023-11-01 10:00:27 +0700

    firebase update

--- client/src/card.js
+++ client/src/card.js
@@ -92,7 +92,7 @@ export default function Album() {
         <Toolbar>
           <QuizIcon sx={{ mr: 2 }} />
           <Typography variant="h6" color="inherit" noWrap>
-            Midterm Exam
+            Midterm Test Exam 
           </Typography>
         </Toolbar>
       </AppBar>
